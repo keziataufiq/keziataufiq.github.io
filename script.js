f1a227b2ddd36d30fc3086ae023a8924a5e939b1
keziataufiq <kezia.taufiq@gmail.com>
2025-09-26 23:11:46 -0700

    Adding local A2 folder to GitHub

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("script.js loaded");
+
+let mainTitle = document.querySelector("#main-title");
+
+mainTitle.addEventListener("click", function() {
+    alert("Hey why are you clicking on me?");
+});
